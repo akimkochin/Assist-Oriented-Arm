d01fad3d387e8f38bcd416500151ae130b862515
kojima <is0165hp@ed.ritsumei.ac.jp>
2017-08-10 18:52:05 +0900

    モータをスピードコントロールからポジションコントロールに変更

--- passive/passive_control.ino
+++ passive/passive_control.ino
@@ -7,14 +7,14 @@
 #define DXL_BUS_SERIAL1 1
 #define P_DX1 1
 #define P_DX2 2
-#define breaking 22
+#define breaking 16
 int breaking_state;
 Dynamixel Dxl(DXL_BUS_SERIAL1);
 void setup() {
   // put your setup code here, to run once:
 	Dxl.begin(3);
-	Dxl.wheelMode(P_DX1);
-	Dxl.wheelMode(P_DX2); //jointMode() is to use position mode
+	Dxl.jointMode(P_DX1);
+	Dxl.jointMode(P_DX2); //jointMode() is to use position mode
 
     pinMode(breaking, INPUT_PULLDOWN);
 
@@ -22,30 +22,41 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly: 
+ breaking_state = digitalRead(breaking);
 
+    /*Turn dynamixel ID 2 to position 0*/
+ if(breaking_state==HIGH){
+    Dxl.goalPosition(P_DX1, 1000); 
+    Dxl.goalPosition(P_DX2, 1010); 
+    // Wait for 1 second (1000 milliseconds)
+    delay(500); 
+ }
+ if(breaking_state==LOW){
+    /*Turn dynamixel ID 2 to position 300*/
+    Dxl.goalPosition(P_DX1, 400); 
+    Dxl.goalPosition(P_DX2, 500);
+    // Wait for 1 second (1000 milliseconds)
+    delay(500);   
+ }
+/*
     breaking_state = digitalRead(breaking);
     if(breaking_state==HIGH){
        //Dxl.setPosition(ID[0],0,100);
        //Dxl.setPosition(ID[1],210,100);
        //new prototype program
-    	Dxl.writeWord(P_DX1, GOAL_SPEED, 1023);
-    	Dxl.writeWord(P_DX2, GOAL_SPEED, 1023);
-        delay(2500);
+        Dxl.goalPosition(P_DX1, 1010); 
+        Dxl.goalPosition(P_DX2, 1010);
+       delay(1000); 
         SerialUSB.println("BREAK_OFF");
-		for(int count = 0;breaking_state == HIGH;count++){
-            Dxl.writeWord(P_DX1, GOAL_SPEED, 0);
-            Dxl.writeWord(P_DX2, GOAL_SPEED, 0);
-            breaking_state = digitalRead(breaking);
-            //SerialUSB.println(count);
-        }
-        Dxl.writeWord(P_DX1, GOAL_SPEED, 1023 | 0x400);
-        Dxl.writeWord(P_DX2, GOAL_SPEED, 1023 | 0x400);
-        delay(2500);
-        Dxl.writeWord(P_DX1, GOAL_SPEED, 0);
-        Dxl.writeWord(P_DX2, GOAL_SPEED, 0);
-        SerialUSB.println("reverse sucsess");
     }
-  
+    if(breaking_state==LOW){
+     Dxl.goalPosition(P_DX1, 1010); 
+     Dxl.goalPosition(P_DX2, 1010); 
+            delay(1000); 
+
+        SerialUSB.println("BREAK_ON");
+    }
+  */
 
 
 }
